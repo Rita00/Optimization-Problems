e3fa8af5fe8b49d22e2e721b912c94ae5d23a482
Rita <rita.lapao00@gmail.com>
2021-02-18 18:01:14 -0100

    Initial possible solution

--- 2048/main.cpp
+++ 2048/main.cpp
@@ -1,4 +1,4 @@
-
+#include <iostream>
 
 
 /*
@@ -16,6 +16,27 @@ class MyClass {       // The class
     string myString;  // Attribute (string variable)
 };
  */
+
+void res(int **board, int size) {
+    char allMoves[] = {'A', 'W', 'S', 'D'}, move = allMoves[rand() % 4];
+    for (int i = 0; i < size; i++) {
+        if (move == 'A'|| move == 'W') {
+            for (int j = size - 1; j <= 0; j++) {
+                int current = board[i][j], bef = board[i][j - 1];
+                if (bef && current && (bef == current)) {
+                    board[i][size - 2] = board[i][size - 1] + board[i][size - 2];
+                    board[i][size - 1] = 0;
+                }
+            }
+        } else {
+            for (int j = 0; j < size; j++) {
+
+            }
+        }
+
+    }
+}
+
 int main() {
     /*
   MyClass myObj;  // Create an object of MyClass
@@ -24,12 +45,12 @@ int main() {
   myObj.myNum = 15;
   myObj.myString = "Some text";
 */
-  // Print attribute values
+    // Print attribute values
     // We probably do not need this but it is faster
     std::ios_base::sync_with_stdio(0);
     std::cin.tie(0);
-      int n;
-      std::cin >> n;
-      std::cout << n << "\n";
-  return 0;
+    int n;
+    std::cin >> n;
+    std::cout << n << "\n";
+    return 0;
 }
