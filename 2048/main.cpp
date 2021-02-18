90d7619a9ba27bb451ab83effa0ccb53bcaa7e13
DylanPerdigao <32035460+DylanPerdigao@users.noreply.github.com>
2021-02-18 19:06:42 +0000

    i/o

--- 2048/main.cpp
+++ 2048/main.cpp
@@ -1,14 +1,21 @@
 
 #include <iostream>
 #include <stdio.h>
+#include <string.h>
+
+using namespace std;
+
+void getInput(string input){
+
+}
 
 int main() {
-
-    std::ios_base::sync_with_stdio(0);
-    std::cin.tie(0);
-
-    int n;
-    std::cin >> n;
-    std::cout << n << "\n";
+	string input;
+	//faster with this lines
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+	//input
+	cin >> input;
+    cout << input << "\n";
     return 0;
 }
