2ebfcac5fe0f5d9e070871fe0125a601a902c44e
Rita <rita.lapao00@gmail.com>
2021-04-09 19:19:10 +0100

    first steps

---
+++ ARChitecture/main.cpp
@@ -0,0 +1,12 @@
+/**
+ * @author Ana Rita Rodrigues -  2018284515
+ * @author Dylan Gonçalves Perdigão - 2018233092
+ */
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    return 0;
+}
