084e63ce77d9b964718e1260ff157a69e493dcd6
Rita <rita.lapao00@gmail.com>
2021-04-09 19:39:50 +0100

    regras

--- ARChitecture/main.cpp
+++ ARChitecture/main.cpp
@@ -7,6 +7,16 @@
 using namespace std;
 
 int main() {
+    int num_legos, h, max_H;
 
     return 0;
 }
+
+/**
+ * Regras!
+ * 1- Arco com dimensão 3 <= K <= num_legos, i = i é o primeiro bloco, i = 2 o segundo até ao último bloco i = K
+ * 2- O primeiro e último blocos devem ser colocados no chão, ou seja, h1 = 0 e hK = 0
+ * 3- O iº bloco deve "tocar" nos seus vizinhos
+ * 4- Os valores de hi devem ser monotonicamnete crescentes, até um certo bloco e decrescescer novamente
+ * 5- Dois blocos consecutivos não podem ter o mesmo valor de hi (a mesma altura)
+ */
