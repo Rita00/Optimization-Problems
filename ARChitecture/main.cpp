e321b8797c3c220f385104119178f55605c0bda6
Rita <rita.lapao00@gmail.com>
2021-04-10 23:23:25 +0100

    quiet

--- ARChitecture/main.cpp
+++ ARChitecture/main.cpp
@@ -3,6 +3,8 @@
  * @author Dylan Gonçalves Perdigão - 2018233092
  */
 #include <iostream>
+#include <vector>
+#include <numeric>
 
 using namespace std;
 
@@ -21,12 +23,43 @@ int mod_sub(int a, int b, int mod) {
     return mod_add(a, -b, mod);
 }
 
-int architecture(int num_blocks, int block_h, int max_H) {
-    increasingState = 0;
-    decreasingState = 0;
 
-    return 0;
+int architecture(int num_blocks, int block_h, int max_H) {
+    int cnt, block_cnt;
+    int available_blocks;
+    vector<int> solutions_per_numBlocks;
+    solutions_per_numBlocks.resize(0);
+    solutions_per_numBlocks.resize(num_blocks);
+
+    if (block_h == max_H) return 0;
+    // Falta verificar se pode continuar a crescer com base na quantidade de blocos que pode meter a descer
+    for (int i = 3; i <= num_blocks; i++) {
+        cnt = 0;
+        for (int j = 0; j < i - 2; j++) {
+            block_cnt = (i / 2) + (i % 2) - 1 + j;
+            available_blocks = num_blocks - (block_cnt + 1);
+            if (block_h + block_cnt == max_H) cnt++;
+            //else if (available_blocks * block_h >= block_cnt + block_h) cnt += 0;
+            else cnt += block_cnt * (block_h - 1);
+        }
+        solutions_per_numBlocks[i - 3] = cnt;
+    }
+    int soma = accumulate(solutions_per_numBlocks.begin(), solutions_per_numBlocks.end(), 0);
+    return soma;
 }
+
+
+/*else {
+            while ((actual_h + block_h <= max_H) && block_cnt >= 0) { // Crescer até atingir limite máximo ou não poder passar desse limite
+                while (increment_h < block_h) {
+                    actual_h += increment_h;
+                    architecture(num_blocks, block_h, max_H, actual_h + increment_h);
+                    increment_h++;
+                }
+                block_cnt--;
+            }
+        }*/
+
 
 int main() {
     int test_cases, num_blocks, block_h, max_H;
