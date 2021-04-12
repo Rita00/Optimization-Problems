3e599335f1ae6aef8c779c1fa6a13f4d09dc7354
Rita <rita.lapao00@gmail.com>
2021-04-12 15:59:52 +0100

    solução com 60 pontos, falta otimizar

--- ARChitecture/main.cpp
+++ ARChitecture/main.cpp
@@ -23,42 +23,63 @@ int mod_sub(int a, int b, int mod) {
     return mod_add(a, -b, mod);
 }
 
-
-int architecture(int num_blocks, int block_h, int max_H) {
-    int cnt, block_cnt;
-    int available_blocks;
-    vector<int> solutions_per_numBlocks;
-    solutions_per_numBlocks.resize(0);
-    solutions_per_numBlocks.resize(num_blocks);
-
-    if (block_h == max_H) return 0;
-    // Falta verificar se pode continuar a crescer com base na quantidade de blocos que pode meter a descer
-    for (int i = 3; i <= num_blocks; i++) {
-        cnt = 0;
-        for (int j = 0; j < i - 2; j++) {
-            block_cnt = (i / 2) + (i % 2) - 1 + j;
-            available_blocks = num_blocks - (block_cnt + 1);
-            if (block_h + block_cnt == max_H) cnt++;
-            //else if (available_blocks * block_h >= block_cnt + block_h) cnt += 0;
-            else cnt += block_cnt * (block_h - 1);
-        }
-        solutions_per_numBlocks[i - 3] = cnt;
+int f(int n, int h, int block_h, const vector<vector<int>> &matrix) {
+    int soma = 0;
+    if (h < block_h || n == 0)
+        return 0;
+    if (matrix[n][h] != -1)
+        return matrix[n][h];
+    if (n == 1 && h == block_h)
+        return 1;
+    for (int i = 1; i < block_h; i++) {
+        soma += f(n - 1, h - i, block_h, matrix);
     }
-    int soma = accumulate(solutions_per_numBlocks.begin(), solutions_per_numBlocks.end(), 0);
     return soma;
 }
 
+int architecture2(int num_blocks, int block_h, int max_H) {
+    vector<vector<int>> matrix;
+    vector<int> solutions_per_numBlocks;
+    solutions_per_numBlocks.resize(num_blocks);
+    //int n = (num_blocks / 2) + (num_blocks % 2) + 1;
 
-/*else {
-            while ((actual_h + block_h <= max_H) && block_cnt >= 0) { // Crescer até atingir limite máximo ou não poder passar desse limite
-                while (increment_h < block_h) {
-                    actual_h += increment_h;
-                    architecture(num_blocks, block_h, max_H, actual_h + increment_h);
-                    increment_h++;
+    matrix.resize(num_blocks, vector<int>(max_H + 1, -1));
+    for (int i = 1; i < num_blocks; i++) {
+        int h_max = block_h + ((i - 1) * (block_h - 1));
+        if (h_max > max_H) h_max = max_H;
+        for (int j = block_h + i - 1; j <= h_max; j++) {
+            matrix[i][j] = f(i, j, block_h, matrix);
+            //printf("%d\t%d\n", i, j);
+        }
+    }
+
+
+    for (int i = 0; i < num_blocks; i++) {
+        for (int j = 0; j < max_H + 1; j++) {
+            if (matrix[i][j] == -1)
+                matrix[i][j] = 0;
+            //printf("%d\t", matrix[i][j]);
+        }
+        //printf("\n");
+    }
+    vector<int> soma_colunas;
+    soma_colunas.resize(max_H + 1);
+    int total = 0;
+    for (int coluna = block_h; coluna <= max_H; coluna++) {
+        for (int linha = 1; linha < num_blocks; linha++) {
+            for (int i = 1; i < block_h; i++) {
+                for (int l = 0; l <= num_blocks - linha; l++) {
+//                    if (l == n - 1 && num_blocks %2 == 1)
+//                        break;
+                    total += f(l, coluna - i, block_h, matrix) * matrix[linha][coluna];
                 }
-                block_cnt--;
             }
-        }*/
+        }
+
+    }
+
+    return total;
+}
 
 
 int main() {
@@ -68,7 +89,7 @@ int main() {
         cin >> num_blocks;
         cin >> block_h;
         cin >> max_H;
-        cout << architecture(num_blocks, block_h, max_H) % 1000000007 << endl;
+        cout << architecture2(num_blocks, block_h, max_H) % 1000000007 << endl;
     }
 
     return 0;
