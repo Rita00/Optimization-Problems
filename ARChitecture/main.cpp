2764c01e9101859a8179619cab35169584c14fa6
Rita <rita.lapao00@gmail.com>
2021-04-12 19:23:01 +0100

    remove 2 for's

--- ARChitecture/main.cpp
+++ ARChitecture/main.cpp
@@ -4,12 +4,8 @@
  */
 #include <iostream>
 #include <vector>
-#include <numeric>
 
 using namespace std;
-
-int increasingState;
-int decreasingState;
 
 int mod_abs(int a, int mod) {
     return ((a % mod) + mod) % mod;
@@ -27,7 +23,7 @@ int f(int n, int h, int block_h, const vector<vector<int>> &matrix) {
     int soma = 0;
     if (h < block_h || n == 0)
         return 0;
-    if (matrix[n][h] != -1)
+    if (matrix[n][h] != 0)
         return matrix[n][h];
     if (n == 1 && h == block_h)
         return 1;
@@ -41,9 +37,8 @@ int architecture2(int num_blocks, int block_h, int max_H) {
     vector<vector<int>> matrix;
     vector<int> solutions_per_numBlocks;
     solutions_per_numBlocks.resize(num_blocks);
-    //int n = (num_blocks / 2) + (num_blocks % 2) + 1;
 
-    matrix.resize(num_blocks, vector<int>(max_H + 1, -1));
+    matrix.resize(num_blocks, vector<int>(max_H + 1, 0));
     for (int i = 1; i < num_blocks; i++) {
         int h_max = block_h + ((i - 1) * (block_h - 1));
         if (h_max > max_H) h_max = max_H;
@@ -54,14 +49,6 @@ int architecture2(int num_blocks, int block_h, int max_H) {
     }
 
 
-    for (int i = 0; i < num_blocks; i++) {
-        for (int j = 0; j < max_H + 1; j++) {
-            if (matrix[i][j] == -1)
-                matrix[i][j] = 0;
-            //printf("%d\t", matrix[i][j]);
-        }
-        //printf("\n");
-    }
     vector<int> soma_colunas;
     soma_colunas.resize(max_H + 1);
     int total = 0;
@@ -69,18 +56,13 @@ int architecture2(int num_blocks, int block_h, int max_H) {
         for (int linha = 1; linha < num_blocks; linha++) {
             for (int i = 1; i < block_h; i++) {
                 for (int l = 0; l <= num_blocks - linha; l++) {
-//                    if (l == n - 1 && num_blocks %2 == 1)
-//                        break;
                     total += f(l, coluna - i, block_h, matrix) * matrix[linha][coluna];
                 }
             }
         }
-
     }
-
     return total;
 }
-
 
 int main() {
     int test_cases, num_blocks, block_h, max_H;
@@ -91,7 +73,6 @@ int main() {
         cin >> max_H;
         cout << architecture2(num_blocks, block_h, max_H) % 1000000007 << endl;
     }
-
     return 0;
 }
 
