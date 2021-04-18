e194727a7f91c86475c628660e2ad18cd777714b
Rita <rita.lapao00@gmail.com>
2021-04-18 19:35:03 +0100

    mts otimizaçoe e mt menos tempo, mas ainda apenas 65pontos

--- ARChitecture/main.cpp
+++ ARChitecture/main.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <vector>
 #include <chrono>
+#include <cmath>
 
 using namespace std;
 
@@ -20,54 +21,59 @@ int mod_sub(int a, int b, int mod) {
     return mod_add(a, -b, mod);
 }
 
-int f(int n, int h, int block_h, vector<vector<int>> &matrix) {
-    int soma = 0;
-    if (matrix[n][h] != -1)
-        return matrix[n][h];
+inline unsigned int f(int n, int h, int block_h, vector<vector<unsigned int>> &matrix) {
+    unsigned int soma = 0;
     if (h < block_h || n == 0) {
         soma = 0;
     } else if (n == 1 && h == block_h) {
         soma = 1;
     } else {
         for (int i = 1; i < block_h; i++) {
-            soma += f(n - 1, h - i, block_h, matrix);
+            soma += matrix[n - 1][h - i] - matrix[n - 2][h - i] % 1000000007;
         }
     }
+    soma += matrix[n - 1][h];
 
-    matrix[n][h] = soma;
+    matrix[n][h] = soma % 1000000007;
     return soma;
 }
 
-int architecture2(int num_blocks, int block_h, int max_H) {
-    vector<vector<int>> matrix;
+unsigned int architecture2(int num_blocks, int block_h, int max_H) {
+    vector<vector<unsigned int>> matrix;
 
-    matrix.resize(num_blocks, vector<int>(max_H + 1, -1));
-    int total = 0;
+    matrix.resize(num_blocks, vector<unsigned int>(max_H + 1, 0));
+    unsigned int total = 0;
     int max_col = min(max_H, block_h + ((num_blocks / 2 + num_blocks % 2) * (block_h - 1)));
-    for (int linha = 1; linha < num_blocks; linha++) {
-        for (int coluna = block_h + linha - 1; coluna <= max_col; coluna++) {
-            int lc = f(linha, coluna, block_h, matrix);
+    for (int coluna = block_h; coluna <= max_col; coluna++) {
+        int blocks_min = ceil((float) (coluna - block_h) / (float) (block_h - 1) + 1);
+        //int blocks_max = min(num_blocks, coluna - block_h + 1 + 1);
+        for (int linha = blocks_min; linha < num_blocks; linha++) {
+            unsigned int lc = f(linha, coluna, block_h, matrix);
             if (lc == 0)
                 continue;
             int soma = 0;
-            int min_blocks = (coluna - block_h) / (block_h - 1);
+            //int min_blocks = (coluna - block_h) / (block_h - 1);
+//            for (int i = 1; i < block_h; i++) {
+//                for (int l = min_blocks; l <= num_blocks - linha; l++) {
+//                    //printf("(%d, %d) * (%d, %d)\n", l, coluna - i, linha, coluna);
+//                    soma += matrix[l][coluna - i];
+//                }
+//            }
+            int remaining_blocks = num_blocks - linha;
+
             for (int i = 1; i < block_h; i++) {
-                for (int l = min_blocks; l <= num_blocks - linha; l++) {
-                    //printf("(%d, %d) * (%d, %d)\n", l, coluna - i, linha, coluna);
-                    soma += f(l, coluna - i, block_h, matrix);
-                }
+                soma += matrix[remaining_blocks][coluna - i] % 1000000007;
             }
-            total += soma * lc;
+            total += soma * (lc - matrix[linha - 1][coluna]);
+            total %= 1000000007;
         }
     }
-    for (int i = 0; i < num_blocks; i++) {
-        for (int j = 0; j < max_H + 1; j++) {
-            if (matrix[i][j] == -1)
-                matrix[i][j] = 0;
-            printf("%d\t", matrix[i][j]);
-        }
-        printf("\n");
-    }
+//    for (int i = 0; i < num_blocks; i++) {
+//        for (int j = 0; j < max_H + 1; j++) {
+//            printf("%u\t", matrix[i][j]);
+//        }
+//        printf("\n");
+//    }
     return total;
 }
 
@@ -84,11 +90,11 @@ int main() {
         cin >> num_blocks;
         cin >> block_h;
         cin >> max_H;
-        cout << architecture2(num_blocks, block_h, max_H) % 1000000007 << endl;
+        cout << architecture2(num_blocks, block_h, max_H) << endl;
     }
     auto t2 = high_resolution_clock::now();
     auto ms_int = duration_cast<milliseconds>(t2 - t1);
-    std::cout << ms_int.count() << "ms\n";
+//    std::cout << ms_int.count() << "ms\n";
     return 0;
 }
 
