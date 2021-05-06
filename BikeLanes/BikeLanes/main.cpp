bf734ae74f12e2c3e449dab05e6a617f65114a66
DylanPerdigao <32035460+DylanPerdigao@users.noreply.github.com>
2021-05-06 18:26:40 +0100

    Update main.cpp

--- BikeLanes/BikeLanes/main.cpp
+++ BikeLanes/BikeLanes/main.cpp
@@ -15,7 +15,7 @@ int main() {
     
     cin >> number_maps;
     
-    while(number_map--){
+    while(number_maps--){
         
         map<pair<int,int>,int> graph;
         cin >> number_vertices >> number_arcs >> number_questions;
@@ -28,3 +28,59 @@ int main() {
     }
     return 0;
 }
+
+/*
+Example Input
+=================
+6
+12 18 4 2 13
+3 24
+4 12
+4 28
+2 53
+5 25
+3 64
+6 32
+6 56
+7 54
+8 69
+7 81
+7 92
+9 10 8 10 7 4
+8 11 2 11 12 12 12 10 3 4 43
+2 13
+3 24
+4 12
+4 28
+4 44
+2 13
+3 24
+1 42
+4 28
+4 43
+2 13
+3 24
+1 42
+4 28
+4 42
+2 13
+3 24
+1 42
+4 28
+4 41
+2 13
+3 24
+1 42
+4 28
+
+
+
+Example output
+=================
+3 6 12 17 0 00
+1 35 5
+1 35
+13 1
+
+ 
+*/
