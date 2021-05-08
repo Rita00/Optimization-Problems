b1e39e8eaef50b7c97db8bce0a38f8b3b480e6c8
Rita00 <rita.lapao00@gmail.com>
2021-05-08 18:16:55 +0100

    find circuits (strongly connected components)

--- BikeLanes/BikeLanes/main.cpp
+++ BikeLanes/BikeLanes/main.cpp
@@ -1,31 +1,110 @@
-
 #include <iostream>
 #include <map>
+#include <vector>
+#include <queue>
+#include <list>
+#include <stack>
+
 
 using namespace std;
+vector<bool> visited;
+map<pair<int, int>, int> graph;
+map<int, list<int>> outNeighbours, inNeighbours;
+map<int, list<int>> components;
+
+void Visit(int u, stack<int> &L) {
+    list<int>::iterator v;
+    if (!visited[u]) {
+        visited[u] = true;
+        for (v = outNeighbours[u].begin(); v != outNeighbours[u].end(); v++) {
+            Visit(*v, L);
+        }
+        L.push(u);
+    }
+}
+
+bool isAssigned(int u) {
+    for (auto const&[key, val] : components) {
+        for (int i : val) {
+            if (i == u) return true;
+        }
+    }
+    return false;
+}
+
+void AssignComponent(int u, int root) {
+    list<int>::iterator v;
+    if (!isAssigned(u)) {
+        components[root].push_back(u);
+        for (v = inNeighbours[u].begin(); v != inNeighbours[u].end(); v++) {
+            AssignComponent(*v, root);
+        }
+    }
+}
+
+void removeSingleComp() {
+    list<int> remove;
+    for (auto const&[key, val] : components) {
+        if (val.size() < 2)
+            remove.push_back(key);
+    }
+    for (auto i : remove)
+        components.erase(i);
+}
+
+void Kosaraju(int numV) {
+    stack<int> L;
+    visited.resize(0);
+    visited.resize(numV);
+    for (int u = 0; u < numV; u++) {
+        Visit(u, L);
+    }
+    while (!L.empty()) {
+        int u = L.top();
+        L.pop();
+        AssignComponent(u, u);
+    }
+    removeSingleComp();
+}
+
+void printComponents() {
+    cout << "\n\n=======================" << endl;
+    for (auto const&[key, val] : components) {
+        cout << key;
+        for (int i : val) {
+            cout << "\t" << i << endl;
+        }
+    }
+}
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    
+
     int number_maps;
     int number_vertices, number_arcs, number_questions;
     int origin, destination, length;
-    
+
     cin >> number_maps;
-    
-    while(number_maps--){
-        
-        map<pair<int,int>,int> graph;
+
+    while (number_maps--) {
+        outNeighbours.clear();
+        inNeighbours.clear();
+        graph.clear();
         cin >> number_vertices >> number_arcs >> number_questions;
-        
-        while(number_arcs--){
+        while (number_arcs--) {
             cin >> origin >> destination >> length;
-            graph[{origin,destination}]=length;
+            origin--;
+            destination--;
+            graph[{origin, destination}] = length;
+            outNeighbours[origin].push_back(destination);
+            inNeighbours[destination].push_back(origin);
         }
-        
+        Kosaraju(number_vertices);
+        printComponents();
     }
+    cout << endl;
     return 0;
 }
 
